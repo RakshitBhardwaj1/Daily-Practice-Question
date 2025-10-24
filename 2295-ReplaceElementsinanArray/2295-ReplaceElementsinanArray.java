e07a9ff4b9a25d21bbe1ad05a0c1a1597fe151f8
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-10-24 15:46:04 +0530

    File updated at 10/24/2025, 3:46:07 PM

--- 2295-ReplaceElementsinanArray/2295-ReplaceElementsinanArray.java
+++ 2295-ReplaceElementsinanArray/2295-ReplaceElementsinanArray.java
@@ -1,15 +1,12 @@
-// Last updated: 10/24/2025, 3:45:52 PM
+// Last updated: 10/24/2025, 3:46:07 PM
 import java.util.*;
 
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        // Store each value’s index
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
-
-        // Perform operations in O(1)
         for (int[] op : operations) {
             int oldVal = op[0];
             int newVal = op[1];
