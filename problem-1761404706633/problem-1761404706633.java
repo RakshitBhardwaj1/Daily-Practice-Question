f68dc60ebbc0889907fdb1c4d56a4ec5a54acde4
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-10-25 20:35:02 +0530

    Create

---
+++ problem-1761404706633/problem-1761404706633.java
@@ -0,0 +1,18 @@
+// Last updated: 10/25/2025, 8:35:06 PM
+class Solution {
+    public String maxSumOfSquares(int num, int sum) {
+        if (sum > 9 * num){
+           return ""; 
+        } 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            int digit = Math.min(9, sum);
+            sb.append(digit);
+            sum -= digit;
+        }
+        if (sum != 0){
+            return "";
+        }
+        return sb.toString();
+    }
+}
