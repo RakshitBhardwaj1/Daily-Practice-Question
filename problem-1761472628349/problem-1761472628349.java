1bf9d62be47fd2598ad1a413fb354f6cc47bd107
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-10-26 15:27:08 +0530

    Create

---
+++ problem-1761472628349/problem-1761472628349.java
@@ -0,0 +1,24 @@
+// Last updated: 10/26/2025, 3:27:08 PM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+    private void backtrack(int[] nums, int target, int start, List<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if (target < 0) return;
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            path.add(nums[i]);
+            backtrack(nums, target - nums[i], i + 1, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
