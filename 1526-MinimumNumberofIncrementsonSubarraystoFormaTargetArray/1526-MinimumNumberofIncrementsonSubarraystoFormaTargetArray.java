d8361c234c639855117c1e80596c226d3a25ac65
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-10-30 11:53:09 +0530

    Create 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray

---
+++ 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -0,0 +1,10 @@
+// Last updated: 10/30/2025, 11:53:13 AM
+class Solution {
+    public int minNumberOperations(int[] target) {
+        final int n=target.length;
+        int ans=target[0];
+        for(int i=1; i<n; i++)
+            ans+=Math.max(target[i]-target[i-1], 0);
+        return ans;
+    }
+}
