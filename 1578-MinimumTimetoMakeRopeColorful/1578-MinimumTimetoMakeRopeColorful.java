f1b9ca96c198925876af3a0bd2bed251fcf0375a
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-11-03 21:30:11 +0530

    Create 1578-MinimumTimetoMakeRopeColorful

---
+++ 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.java
@@ -0,0 +1,18 @@
+// Last updated: 11/3/2025, 9:30:14 PM
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = colors.length(), sum = 0;
+        for (int i = 1; i < n; i++) {
+            int maxi = 0;
+            while (i < n && colors.charAt(i) == colors.charAt(i - 1)) {
+                sum += neededTime[i - 1];
+                maxi = Math.max(maxi, neededTime[i - 1]);
+                i++;
+            }
+            sum += neededTime[i - 1];
+            maxi = Math.max(maxi, neededTime[i - 1]);
+            if (maxi != 0) sum -= maxi;
+        }
+        return sum;
+    }
+}
