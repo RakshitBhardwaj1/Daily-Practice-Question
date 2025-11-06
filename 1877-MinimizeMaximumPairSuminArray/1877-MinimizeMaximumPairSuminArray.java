bb055e653618cdaa1339415240d5486c9413c1c3
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-11-06 23:00:48 +0530

    Create 1877-MinimizeMaximumPairSuminArray

---
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -0,0 +1,23 @@
+// Last updated: 11/6/2025, 11:00:50 PM
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        int[] arr = new int[n];
+        for (int i = 2; i < n; i++) {
+            arr[i] = 1;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (arr[i] == 1) {
+                for (int j = i * i; j < n; j += i) {
+                    arr[j] = 0;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (arr[i] == 1) count++;
+        }
+        return count;
+    }
+}
