4b4f990d56f1d3d966f6d1babf0e4f50e69545ed
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2025-11-07 21:22:46 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.java
@@ -0,0 +1,23 @@
+// Last updated: 11/7/2025, 9:22:49 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+                
+            }
+            else{
+                high=mid-1;
+                
+            }
+        }
+        return -1;
+    }
+}
