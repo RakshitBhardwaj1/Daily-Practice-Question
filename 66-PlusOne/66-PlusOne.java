343a3bb61133b0e8377f0b135e3f6c01a728e3b5
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-01-01 13:34:11 +0530

    File updated at 1/1/2026, 1:34:18 PM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,16 +1,16 @@
-// Last updated: 10/24/2025, 10:37:42 PM
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        int[] result = new int[n + 1];
-        result[0] = 1;
-        return result;
-    }
-}
+// Last updated: 1/1/2026, 1:34:18 PM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        int n = digits.length;
+4        for (int i = n - 1; i >= 0; i--) {
+5            if (digits[i] < 9) {
+6                digits[i]++;
+7                return digits;
+8            }
+9            digits[i] = 0;
+10        }
+11        int[] result = new int[n + 1];
+12        result[0] = 1;
+13        return result;
+14    }
+15}
