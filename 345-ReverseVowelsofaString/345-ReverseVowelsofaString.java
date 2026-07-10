b5a6b4a4c4f266ab1d3b619e133828cc865ee6aa
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-07-10 17:04:32 +0530

    File updated at 7/10/2026, 5:04:33 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -1,40 +1,28 @@
-// Last updated: 7/10/2026, 5:03:25 PM
+// Last updated: 7/10/2026, 5:04:33 PM
 1class Solution {
 2    public String reverseVowels(String s) {
-3        int n = s.length();
-4        char[] arr = s.toCharArray();
-5
-6        int i = 0;
-7        int j = n - 1;
-8
-9        while (i < j) {
-10
-11            if (arr[i] != 'a' && arr[i] != 'e' &&
-12                arr[i] != 'i' && arr[i] != 'o' &&
-13                arr[i] != 'u' && arr[i] != 'A' &&
-14                arr[i] != 'E' && arr[i] != 'I' &&
-15                arr[i] != 'O' && arr[i] != 'U') {
-16
-17                i++;
-18            }
-19            else if (arr[j] != 'a' && arr[j] != 'e' &&
-20                     arr[j] != 'i' && arr[j] != 'o' &&
-21                     arr[j] != 'u' && arr[j] != 'A' &&
-22                     arr[j] != 'E' && arr[j] != 'I' &&
-23                     arr[j] != 'O' && arr[j] != 'U') {
-24
-25                j--;
-26            }
-27            else {
-28                char ch = arr[i];
-29                arr[i] = arr[j];
-30                arr[j] = ch;
-31
-32                i++;
-33                j--;
-34            }
-35        }
-36
-37        return new String(arr);
-38    }
-39}
+3        int n=s.length();
+4        char[] arr=s.toCharArray();
+5        int i=0;
+6        int j=n-1;
+7        while(i<j){
+8            if(arr[i]!='a'&& arr[i]!= 'e'&& arr[i]!='i'&& arr[i]!='o' && arr[i]!='u' && arr[i]!='A'&& arr[i]!= 'E'&& arr[i]!='I'&& arr[i]!='O'&& arr[i]!='U'){
+9                i++;
+10            }
+11            else if(arr[j]!='a'&& arr[j]!= 'e'&& arr[j]!='i'&& arr[j]!='o'&& arr[j]!='u'&& arr[j]!='A'&& arr[j]!= 'E'&& arr[j]!='I'&& arr[j]!='O' && arr[j]!='U'){
+12                j--;
+13            }
+14            else{
+15
+16            
+17            char ch=arr[i];
+18            arr[i]=arr[j];
+19            arr[j]=ch;
+20
+21            i++;
+22            j--;
+23            }
+24        }
+25        return new String(arr);
+26    }
+27}
