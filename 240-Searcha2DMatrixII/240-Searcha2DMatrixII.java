c962e7c2e5d3c2cf9223298c0d323c66c812d98d
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-07-20 12:19:53 +0530

    File updated at 7/20/2026, 12:19:57 PM

--- 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
@@ -1,15 +1,25 @@
-// Last updated: 10/24/2025, 10:14:26 AM
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-}
+// Last updated: 7/20/2026, 12:19:57 PM
+1class Solution {
+2    public boolean searchMatrix(int[][] matrix, int target) {
+3        int n=matrix.length;
+4        for(int[] arr:matrix){
+5            int low=0;
+6            int high=arr.length;
+7            while(low<high){
+8                int mid=low+(high-low)/2;
+9                if(arr[mid]==target){
+10                    return true;
+11                }
+12                if(arr[mid]<target){
+13                    low=mid+1;
+14                }
+15                else{
+16                    high=mid;
+17                }
+18            }
+19        }
+20        return false;
+21
+22    }
+23    
+24}
