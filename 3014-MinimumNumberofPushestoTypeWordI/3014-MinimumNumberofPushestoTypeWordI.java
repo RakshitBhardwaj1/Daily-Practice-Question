8c9713a04d2c82071ba0ddf417a4eab3311a5add
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-07-30 09:36:23 +0530

    Create 3014-MinimumNumberofPushestoTypeWordI

---
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
@@ -0,0 +1,10 @@
+// Last updated: 7/30/2026, 9:36:26 AM
+1class Solution {
+2    public int minimumPushes(String word) {
+3        int ans=0;
+4        for(int i=0;i<word.length();i++){
+5            ans+=(i/8)+1;
+6        }
+7        return ans;
+8    }
+9}
