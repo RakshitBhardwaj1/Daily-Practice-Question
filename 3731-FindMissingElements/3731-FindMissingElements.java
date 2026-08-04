b00d537640942d91fa3cf12e3644c5b8b598775d
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-08-04 16:23:26 +0530

    File updated at 8/4/2026, 4:23:29 PM

--- 3731-FindMissingElements/3731-FindMissingElements.java
+++ 3731-FindMissingElements/3731-FindMissingElements.java
@@ -1,20 +1,12 @@
-// Last updated: 8/4/2026, 4:05:02 PM
+// Last updated: 8/4/2026, 4:23:29 PM
 1class Solution {
-2    public List<Integer> findMissingElements(int[] nums) {
-3        int min=Integer.MAX_VALUE;
-4        int max=Integer.MIN_VALUE;
-5        HashSet<Integer> set=new HashSet<>();
-6        for(int i=0;i<nums.length;i++){
-7            min=Math.min(min,nums[i]);
-8            max=Math.max(max,nums[i]);
-9            set.add(nums[i]);
-10        }
-11        ArrayList<Integer> l=new ArrayList<>();
-12        for(int i=min;i<max;i++){
-13            if(!set.contains(i)){
-14                l.add(i);
-15            }
-16        }
-17        return l;
-18    }
-19}
+2    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+3        Arrays.sort(arr);
+4        int n=arr.length;
+5        arr[0]=1;
+6        for(int i=1;i<n;i++){
+7            arr[i]=Math.min(arr[i],arr[i-1]+1);
+8        }
+9        return arr[n-1];
+10    }
+11}
