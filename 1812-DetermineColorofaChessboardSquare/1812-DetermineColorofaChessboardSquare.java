c1f053e7dbbcdd14ffbc38bf0ec0ceb6e27018db
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-08-09 15:51:51 +0530

    File updated at 8/9/2026, 3:51:52 PM

--- 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.java
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.java
@@ -1,11 +1,13 @@
-// Last updated: 8/9/2026, 3:45:54 PM
+// Last updated: 8/9/2026, 3:51:52 PM
 1class Solution {
-2    public boolean squareIsWhite(String coordinates) {
-3        int c=coordinates.charAt(0)-'a';
-4        int n=coordinates.charAt(1)-'0';
-5        if(c%2==n%2){
-6            return true;
-7        }
-8        return false; 
-9    }
-10}
+2    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+3        int c1=coordinate1.charAt(0)-'a';
+4        int c2=coordinate2.charAt(0)-'a';
+5        int n1=coordinate1.charAt(1)-'0';
+6        int n2=coordinate2.charAt(1)-'0';
+7        if((c1+n1)%2==(c2+n2)%2){
+8            return true;
+9        }
+10        return false;
+11    }
+12}
