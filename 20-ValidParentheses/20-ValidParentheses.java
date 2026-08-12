a0273d1e324b92fe62291d904c8d2d6431ac31b3
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-08-12 18:58:36 +0530

    File updated at 8/12/2026, 6:58:40 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,22 +1,26 @@
-// Last updated: 1/31/2026, 9:38:01 AM
+// Last updated: 8/12/2026, 6:58:40 PM
 1class Solution {
 2    public boolean isValid(String s) {
-3        Stack<Character> st = new Stack<>();
-4
-5        for(int i = 0; i < s.length(); i++){
-6            char ch = s.charAt(i);
-7
-8            if(ch == '('|| ch=='[' || ch=='{'){
-9                st.push(ch);
-10            } else {
-11                if(st.isEmpty()) return false;
-12                char top=st.pop();
-13                if(ch==')' && top!='(') return false;
-14                if(ch==']' && top!='[') return false;
-15                if(ch=='}' && top!='{') return false;
-16            }
-17        }
-18        return st.isEmpty();
-19    }
-20}
-21
+3        int n=s.length();
+4        Stack<Character> st=new Stack<>();
+5        if(s.charAt(0)==')'||s.charAt(0)==']'||s.charAt(0)=='}'){
+6            return false;
+7        }
+8        for(int i=0;i<n;i++){
+9            char ch=s.charAt(i);
+10            if(ch=='('||ch=='['||ch=='{'){
+11                st.push(ch);
+12            }
+13            else{
+14                if(st.isEmpty()){
+15                    return false;
+16                }
+17                if(ch==')'&&st.peek()!='(' || ch==']'&&st.peek()!='[' || ch=='}'&&st.peek()!='{'){
+18                    return false;
+19                }
+20                st.pop();
+21            }
+22        }
+23        return st.isEmpty();
+24    }
+25}
