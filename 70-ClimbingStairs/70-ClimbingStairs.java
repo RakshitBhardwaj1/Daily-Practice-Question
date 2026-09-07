0b874f43228b4e798cc62786842048c21c68fc6f
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-09-07 17:56:29 +0530

    File updated at 9/7/2026, 5:56:36 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,16 +1,17 @@
-// Last updated: 9/7/2026, 4:43:02 PM
+// Last updated: 9/7/2026, 5:56:36 PM
 1class Solution {
-2    public int climbStairs(int n) {
-3        if(n==0) return 1;
-4        if(n==1) return 1;
+2    public int minCostClimbingStairs(int[] cost) {
+3        if(cost.length==1) return cost[0];
+4        if(cost.length==2) return Math.min(cost[0],cost[1]);
 5
-6        int prev1=1;
-7        int prev2=1;
-8        for(int i=2;i<=n;i++){
-9            int c=prev1+prev2;
-10            prev2=prev1;
-11            prev1=c;
-12        }
-13        return prev1;
-14    }
-15}
+6        int cost1=cost[0];
+7        int cost2=cost[1];
+8
+9        for(int i=2;i<cost.length;i++){
+10            int cur=Math.min(cost1,cost2)+cost[i];
+11            cost1=cost2;
+12            cost2=cur;
+13        }
+14        return Math.min(cost1,cost2);
+15   }
+16}
