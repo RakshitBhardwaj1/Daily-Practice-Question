86b7f44b5ed25098b52e8f063bddce311572bda7
Rakshit Bhardwaj <142593633+RakshitBhardwaj1@users.noreply.github.com>
2026-09-26 15:32:44 +0530

    File updated at 9/26/2026, 3:32:44 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,13 +1,25 @@
-// Last updated: 9/26/2026, 12:57:27 PM
+// Last updated: 9/26/2026, 3:32:44 PM
 1class Solution {
-2    public boolean canJump(int[] nums) {
+2    public int jump(int[] nums) {
 3        int n=nums.length;
-4        int d=n-1;
-5        for(int i=n-2;i>=0;i--){
-6            if(i+nums[i]>=d){
-7                d=i;
-8            }
-9        }
-10        return d==0;
-11    }
-12}
+4        int totaljump=0;
+5        int destination=n-1;
+6        int coverage=0;
+7        int lastjumpidx=0;
+8        if(n==1){
+9            return 0;
+10        }
+11        for(int i=0;i<n;i++){
+12            coverage=Math.max(coverage,i+nums[i]);
+13            if(i==lastjumpidx){
+14                lastjumpidx=coverage;
+15                totaljump++;
+16
+17                if(coverage>=destination){
+18                    return totaljump;
+19                }
+20            }
+21        }
+22        return totaljump;
+23    }
+24}
